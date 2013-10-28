3a68161f749af28cb7023532f36453d09d845c4b
Florian Lentsch <office@florian-lentsch.at>
2013-10-28 14:13:40 +0100

    started with db diagram

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!
   
   def show
     render json: {status: 'success', article: {name: 'Test', description: params[:barcode], barcode: params[:barcode]}}
