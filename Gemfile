c7ffb9d0ba1dad5485ed79a374f2d99240982ed6
Florian Lentsch <office@florian-lentsch.at>
2013-11-04 16:50:42 +0100

    played around with remote barcode fetching

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'clerk', git: "git://github.com/house9/clerk.git"
 #views
 gem 'haml'
 
+gem 'nokogiri'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Article < ActiveRecord::Base
   track_who_does_it :creator_foreign_key => "creator_id", :updater_foreign_key => "modifier_id"
   
@@ -6,7 +8,18 @@ class Article < ActiveRecord::Base
     article = self.find_by(barcode: data[:barcode], creator_id: current_user.id) unless current_user.nil?
     article = self.find_by(barcode: data[:barcode]) if article.nil?
     
-    article = self.new(data) if article.nil?
+    if article.nil?
+      article = self.new(data)
+      
+      if article.name.nil? or article.name.empty?
+        url = "http://www.barcoo.com/#{data[:barcode]}?source=pb"
+        Rails.logger.info "Fetching: " + url
+        doc = Nokogiri::HTML(open(url))
+        elem = doc.at_css('div#baseDataHolder h1') unless doc.nil?
+        article.name = elem.text unless elem.nil?
+        Rails.logger.info "Done fetching: " + url
+      end
+    end
     
     article
   end
