83a58c66733ae3d4c78f6973af129883f8effa02
Florian Lentsch <office@florian-lentsch.at>
2015-07-17 16:28:00 +0200

    removed byebug from requirements

--- app/controllers/product_entries_controller.rb
+++ app/controllers/product_entries_controller.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 class ProductEntriesController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource  only: [:destroy]
--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 FactoryGirl.define do
   factory :user do
     username  "test"
