26b30abd12da83b6b33f60c32e9242052a087727
Florian Lentsch <office@florian-lentsch.at>
2015-07-17 16:25:19 +0200

    progress3 with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ set :repo_url, 'git@flolab.florian-lentsch.at:mobile-apps/bbw_server.git'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
