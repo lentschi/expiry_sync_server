f633d3625567c7ac8d5513be6c7121d33f3ae5a0
Florian Lentsch <office@florian-lentsch.at>
2016-01-11 14:43:09 +0100

    removed some fetchers

--- config/initializers/remote_article_fetcher.rb
+++ config/initializers/remote_article_fetcher.rb
@@ -2,6 +2,6 @@ RemoteArticleFetcher.setup do |config|
   if Rails.env.test?
     config.fetcher_sequence = [:testing]
   else
-    config.fetcher_sequence = [:barcoo, :codecheck_info, :open_gtin_ean_db]
+    config.fetcher_sequence = []
   end
 end
