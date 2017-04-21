067590d079bc61d1ad73927fa394d8d7902fb148
Florian Lentsch <office@florian-lentsch.at>
2017-04-21 09:14:46 +0200

    enabled PUT for CORS

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module ExpirySyncServer
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
