310504615fe82750f0b1d4017a758d5777053fed
Florian Lentsch <office@florian-lentsch.at>
2017-05-05 11:03:01 +0200

    removed testing from fetchers (accidental commit)

--- config/initializers/remote_article_fetcher.rb
+++ config/initializers/remote_article_fetcher.rb
@@ -2,6 +2,6 @@ RemoteArticleFetcher.setup do |config|
   if Rails.env.test?
     config.fetcher_sequence = [:testing]
   else
-    config.fetcher_sequence = [:testing] # TODO-no-commit
+    config.fetcher_sequence = []
   end
 end
