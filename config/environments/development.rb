e21f58261b83de909aacb765f405d5fcd0884dc7
Florian Lentsch <office@florian-lentsch.at>
2017-07-20 08:23:54 +0200

    enabled EMAIL_LINK_HOST env var for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ ExpirySyncServer::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: (ENV['EMAIL_LINK_HOST'].nil? ? 'localhost:3000' : ENV['EMAIL_LINK_HOST']) }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
