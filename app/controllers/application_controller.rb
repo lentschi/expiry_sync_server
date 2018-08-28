1b4499949f598bce37c54898539a1af04fcd419f
Florian Lentsch <office@florian-lentsch.at>
2018-08-28 20:21:43 +0200

    Fix for redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,8 +52,10 @@ class ApplicationController < ActionController::Base
   def set_redirect_url_header
     return if request.headers['X-Expiry-Sync-Api-Version'].nil? or request.headers['X-Expiry-Sync-Api-Version'].to_i < 2
     redirect_setting = ApplicationSetting.find_by_setting_key('redirect_url')
-    response.header['Access-Control-Expose-Headers'] = 'X-Expiry-Sync-Permanent-Redirect'
-    response.header['X-Expiry-Sync-Permanent-Redirect'] = redirect_setting.setting_value unless redirect_setting.nil?
-    render json: {status: 'Expiry-Sync-Permanent-Redirect'}
+    unless redirect_setting.nil?
+      response.header['Access-Control-Expose-Headers'] = 'X-Expiry-Sync-Permanent-Redirect'
+      response.header['X-Expiry-Sync-Permanent-Redirect'] = redirect_setting.setting_value
+      render json: {status: 'Expiry-Sync-Permanent-Redirect'}
+    end
   end
 end
