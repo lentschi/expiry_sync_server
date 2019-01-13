6b34484395b8b2317850b35c75d66f309844b847
Florian Lentsch <office@florian-lentsch.at>
2019-01-13 17:26:39 +0100

    Development log & mysql default db

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,4 +30,6 @@ ExpirySyncServer::Application.configure do
   config.assets.debug = true
 
   config.secret_key_base = 'c5fc7721e07c546e0d0949d4059f8bb1e39f26a3be6b1d5ec03eff2629d8910ded026afdab2dedefcd9a7f57bf95034adcb19ba4ef8b0a8e76a1a1e0113a4295'
+
+  config.logger = Logger.new(STDOUT)
 end
